7149457ae579693419e817f2a7da388ca6377b50
rstrozensky <ryanstrozensky@yahoo.com>
2019-02-19 10:28:16 -0500

    Made ConfigurationViewModel extend AndroidViewModel

--- app/src/main/java/com/galactichitchhiker/spacetrader/viewmodels/ConfigurationViewModel.java
+++ app/src/main/java/com/galactichitchhiker/spacetrader/viewmodels/ConfigurationViewModel.java
@@ -1,4 +1,19 @@
 package com.galactichitchhiker.spacetrader.viewmodels;
 
-public class ConfigurationViewModel {
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+import com.galactichitchhiker.spacetrader.models.Game;
+
+public class ConfigurationViewModel extends AndroidViewModel {
+
+    public ConfigurationViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public void createPlayer(String name, int pilotPoints, int engineerPoints, int traderPoints,
+                             int fighterPoints, Game.GameDifficulty difficulty) {
+
+    }
 }
