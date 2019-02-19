6ff6e74df3c2c037481426d133da3879e4799201
Jinchen Ma <jma337@gatech.edu>
2019-02-19 11:00:25 -0500

    Added the createModel class ComfigurationViewModel.java class in viewmodels

--- app/src/main/java/com/galactichitchhiker/spacetrader/viewmodels/ConfigurationViewModel.java
+++ app/src/main/java/com/galactichitchhiker/spacetrader/viewmodels/ConfigurationViewModel.java
@@ -5,15 +5,26 @@ import android.arch.lifecycle.AndroidViewModel;
 import android.support.annotation.NonNull;
 
 import com.galactichitchhiker.spacetrader.models.Game;
+import com.galactichitchhiker.spacetrader.models.Model;
 
 public class ConfigurationViewModel extends AndroidViewModel {
 
     public ConfigurationViewModel(@NonNull Application application) {
         super(application);
     }
+    private Model model;
 
-    public void createPlayer(String name, int pilotPoints, int engineerPoints, int traderPoints,
-                             int fighterPoints, Game.GameDifficulty difficulty) {
-
+    public String createModel(String name, int pilotPoints, int engineerPoints, int traderPoints,
+                            int fighterPoints, Game.GameDifficulty difficultyLevel) {
+        int pointSum = pilotPoints + engineerPoints + traderPoints + fighterPoints;
+        if (pointSum == 16 && name != null) {
+            model = new Model(name, pilotPoints, engineerPoints, traderPoints, fighterPoints, difficultyLevel);
+            return "success";
+        } else if (pointSum != 16) {
+            return "the total skill points have to be 16";
+        } else if (name == null) {
+            return "the name of the player can not be null";
+        }
+        return "some kind of error";
     }
 }
