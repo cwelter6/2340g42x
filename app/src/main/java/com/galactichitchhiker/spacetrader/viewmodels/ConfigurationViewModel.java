26d347f0efee043d11b0d3239ba00fd4e84efdc9
Jinchen Ma <jma337@gatech.edu>
2019-02-21 10:22:11 -0500

    Fixed the bug that happens when player's name is empty

--- app/src/main/java/com/galactichitchhiker/spacetrader/viewmodels/ConfigurationViewModel.java
+++ app/src/main/java/com/galactichitchhiker/spacetrader/viewmodels/ConfigurationViewModel.java
@@ -57,10 +57,10 @@ public class ConfigurationViewModel extends AndroidViewModel {
         if (pointSum == MAXIMUM_SKILL_POINTS && name != null) {
             model = new Model(name, pilotPoints, engineerPoints, traderPoints, fighterPoints, difficultyLevel);
             return "success";
+        } else if (name.length() == 0) {
+            return "Error: The name of the player can not be empty";
         } else if (pointSum != MAXIMUM_SKILL_POINTS) {
             return "Error: The total skill points have to be 16";
-        } else if (name == null) {
-            return "Error: The name of the player can not be null";
         }
         return "An error occured...";
     }
