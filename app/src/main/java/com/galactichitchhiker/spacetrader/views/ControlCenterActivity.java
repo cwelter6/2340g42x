c5a7c159524bd2b3be00955ed18a401dd1d4e03c
Adeeb Zaman <zamanadeeb789@gmail.com>
2019-02-26 11:52:20 -0500

    Added blank control center screen

---
+++ app/src/main/java/com/galactichitchhiker/spacetrader/views/ControlCenterActivity.java
@@ -0,0 +1,15 @@
+package com.galactichitchhiker.spacetrader.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.galactichitchhiker.spacetrader.R;
+
+public class ControlCenterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control_center);
+    }
+}
--- app/src/main/java/com/galactichitchhiker/spacetrader/views/PlayerInformationActivity.java
+++ app/src/main/java/com/galactichitchhiker/spacetrader/views/PlayerInformationActivity.java
@@ -2,9 +2,12 @@ package com.galactichitchhiker.spacetrader.views;
 
 import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.galactichitchhiker.spacetrader.R;
@@ -50,7 +53,24 @@ public class PlayerInformationActivity extends AppCompatActivity {
         //
         gameDifficultyText.setText(viewModel.getGame().getDifficultyLevel().toString());
 
+        Button startButton = findViewById(R.id.start_button);
 
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onStartPressed(view);
+            }
+        });
+    }
+
+
+    /**
+     * Go to control center screen
+     * @param view
+     */
+    public void onStartPressed(View view) {
+        Intent intent = new Intent(PlayerInformationActivity.this, ControlCenterActivity.class);
+        startActivity(intent);
     }
 
 }
