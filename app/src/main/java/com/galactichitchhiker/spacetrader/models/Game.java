1a8e3c69cd665e4daf682a999c5d856fe966de43
Jinchen Ma <jma337@gatech.edu>
2019-03-27 23:01:42 -0400

    Merge branch 'master' of D:\GT courses\2019 Spring\CS 2340\SpaceTrader with conflicts.

--- app/src/main/java/com/galactichitchhiker/spacetrader/models/Game.java
+++ app/src/main/java/com/galactichitchhiker/spacetrader/models/Game.java
@@ -1,15 +1,55 @@
 package com.galactichitchhiker.spacetrader.models;
 
+
+/**
+ * Holds current player object and all other information needed to set up a game
+ * 
+ */
 public class Game {
-
-    public enum GameDifficulty {
-        EASY, NORMAL, HARD, DIFFICULT
-    };
-
+    
+    private Player player;
+    private Universe universe;
     private GameDifficulty difficultyLevel = GameDifficulty.NORMAL;
 
-    public Game(GameDifficulty difficultyLevel) {
+
+    /**
+     * Constructs a new game object
+     * 
+     * @param p               - Player object
+     * @param difficultyLevel - game difficulty level
+     */
+    public Game(Player p, GameDifficulty difficultyLevel){
         this.difficultyLevel = difficultyLevel;
+        player = p;
+
+        universe = new Universe();
     }
+
+    /**
+     * Enum for game difficulty level
+     */
+    private enum GameDifficulty {
+        EASY, NORMAL, HARD, DIFFICULT
+    }
+
+    /**
+     * Get Player instance
+     *
+     * @return player
+     */
+    public Player getPlayer() {
+        return player;
+    }
+
+    /**
+     * Get game difficulty
+     *
+     * @return GameDifficulty
+     */
+    public GameDifficulty getDifficultyLevel() {
+        return difficultyLevel;
+    }
+
+
 
 }
