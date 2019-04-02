6b4207cc18282968163c0a974ecc014f33f97957
Jinchen Ma <jma337@gatech.edu>
2019-04-02 11:16:37 -0400

    added javadocs to the Ship class

--- app/src/main/java/com/galactichitchhiker/spacetrader/models/Ship.java
+++ app/src/main/java/com/galactichitchhiker/spacetrader/models/Ship.java
@@ -51,6 +51,10 @@ public class Ship {
          }
     }
 
+    /**
+     * subtract fuel from the ship
+     * @param sub amount substracted
+     */
     public void subtractFuel(double sub) {
         if (fuel - sub < 0) {
             fuel = 0;
@@ -59,18 +63,36 @@ public class Ship {
         }
     }
 
+    /**
+     * get fuel
+     * @return fuel
+     */
     public double getFuel() {
         return fuel;
     }
 
+    /**
+     * get the max amount fuel we can have
+     * @return max amount of fuel we can have
+     */
     public double getFuelCapacity() {
         return fuelCapacity;
     }
 
+    /**
+     * get the amount of cargo of type g we have
+     * @param g the type of the trade good
+     * @return the amount of type g good we have
+     */
     public int getCargoAmountOf(TradeGoods g) {
         return cargo.get(g);
     }
 
+    /**
+     * add cargo
+     * @param g the type of goods to be added
+     * @param num the number of goods to be added
+     */
     public void addCargoOf(TradeGoods g, int num) {
 
 
@@ -83,6 +105,11 @@ public class Ship {
         usedCargoSpace += num;
     }
 
+    /**
+     * remove cargo
+     * @param g the type of goods to be removed
+     * @param num the number of goods to be removed
+     */
     public void removeCargoOf(TradeGoods g, int num) {
 
         if (cargo.get(g) < num) {
@@ -94,14 +121,26 @@ public class Ship {
         usedCargoSpace -= num;
     }
 
+    /**
+     * get the cargo space already used
+     * @return the cargo space already used
+     */
     public int getUsedCargoSpace() {
         return usedCargoSpace;
     }
 
+    /**
+     * get Max Cargo Space
+     * @return Max Cargo Space
+     */
     public int getMaxCargoSpace() {
         return maxCargoSpace;
     }
 
+    /**
+     * get Remaining Cargo Space
+     * @return Remaining Cargo Space
+     */
     public int getRemainingCargoSpace(){
         return maxCargoSpace - usedCargoSpace;
     }
